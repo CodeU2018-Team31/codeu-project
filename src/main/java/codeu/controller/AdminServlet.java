0d25b09a91e7903f7b82e3959c7df06e3384a934
Ivanna <ivannpe@gmail.com>
2018-05-23 12:47:14 -0400

    Defined Admin servlet and jsp, edited login servlet to allow admin login to redirect to /admin at the right time

--- src/main/java/codeu/controller/AdminServlet.java
+++ src/main/java/codeu/controller/AdminServlet.java
@@ -13,8 +13,6 @@
 // limitations under the License.
 package codeu.controller;
 
-import codeu.model.data.User;
-import codeu.model.store.basic.UserStore;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,44 +22,12 @@ import java.io.IOException;
 
 public class AdminServlet extends HttpServlet {
 
-    /**
-     * Store class that gives access to Users.
-     */
-    private UserStore userStore;
 
     @Override
     public void init() throws ServletException {
         super.init();
-        setUserStore(UserStore.getInstance());
     }
 
-    /**
-     * Sets the UserStore used by this servlet. This function provides a common setup method for use
-     * by the test framework or the servlet's init() function.
-     */
-    void setUserStore(UserStore userStore) {
-        this.userStore = userStore;
-    }
-//
-//    @Override
-//    public void doPost(HttpServletRequest request, HttpServletResponse response)
-//        throws IOException, ServletException {
-//
-//        String username = (String) request.getSession().getAttribute("user");
-//        if (username != "admin") {
-//            // user is not logged in, don't let them go to admin page
-//            response.sendRedirect("/login");
-//            return;
-//        }
-//
-//        User user = userStore.getUser(username);
-//        if (user == null) {
-//            // user was not found, don't let them go to admin page
-//            System.out.println("User not found: " + username);
-//            response.sendRedirect("/login");
-//            return;
-//        }
-//    }
 
     /**
      * This function fires when a user navigates to the Admin page.
@@ -72,7 +38,7 @@ public class AdminServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
         String username = (String) request.getSession().getAttribute("user");
-//        User user = userStore.getUser(username);
+
         if (!username.matches("admin")) {
             response.sendRedirect("/login");
         }else {
@@ -80,9 +46,3 @@ public class AdminServlet extends HttpServlet {
         }
     }
 }
-//    @Override
-//    public void doGet(HttpServletRequest request, HttpServletResponse response)
-//        throws IOException, ServletException {
-//        request.getRequestDispatcher("/WEB-INF/view/admin.jsp").forward(request, response);
-//        }
-//    }
--- src/test/java/codeu/controller/AdminServletTest.java
+++ src/test/java/codeu/controller/AdminServletTest.java
@@ -11,41 +11,41 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//package codeu.controller;
+package codeu.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 //
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.mockito.Mockito;
-//
-//import javax.servlet.RequestDispatcher;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-////
-//public class AdminServletTest {
-//    private AdminServlet adminServlet;
-//    private HttpServletRequest mockRequest;
-//    private HttpServletResponse mockResponse;
-//    private RequestDispatcher mockRequestDispatcher;
-//
-//    @Before
-//    public void setup() {
-//        adminServlet = new AdminServlet();
-//
-//        mockRequest = Mockito.mock(HttpServletRequest.class);
-//        mockResponse = Mockito.mock(HttpServletResponse.class);
-//
-//        mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
-//        Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/admin.jsp"))
-//                .thenReturn(mockRequestDispatcher);
-//    }
-//
-//    @Test
-//    public void testDoGet() throws IOException, ServletException {
-//        adminServlet.doGet(mockRequest, mockResponse);
-//        //verify request dispatcher's forward method is called with the mockRequest and mockResponse
-//        Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
-//    }
-//}
+public class AdminServletTest {
+    private AdminServlet adminServlet;
+    private HttpServletRequest mockRequest;
+    private HttpServletResponse mockResponse;
+    private RequestDispatcher mockRequestDispatcher;
+
+    @Before
+    public void setup() {
+        adminServlet = new AdminServlet();
+        mockRequest = Mockito.mock(HttpServletRequest.class);
+        mockResponse = Mockito.mock(HttpServletResponse.class);
+
+        mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
+        Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/admin.jsp"))
+                .thenReturn(mockRequestDispatcher);
+    }
+
+    @Test
+    public void testDoGet() throws IOException, ServletException {
+        adminServlet.doGet(mockRequest, mockResponse);
+        //verify request dispatcher's forward method is called with the mockRequest and mockResponse
+        Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+    }
+
+}
 
